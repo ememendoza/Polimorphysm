fbefa249dabadfae8e19a94bdd20ecec73d2e4ee
Mario Mendoza <ememendozar002@gmail.com>
2024-03-03 14:27:37 -0500

    Feat: Main method

--- Animals/Program.cs
+++ Animals/Program.cs
@@ -3,51 +3,55 @@
 int option;
 Animal animal;
 
-do
+void Main()
 {
-    Console.WriteLine(
-        "Please select a option\n" +
-        "1 ---- Dog\n" +
-        "2 ---- Cat \n" +
-        "3 ---- Rooster\n" +
-        "4 ---- Cow\n" +
-        "0 ---- Exit"
-    );
-    
-    option = int.Parse(Console.ReadLine());
-    
-    Console.Clear();
-
-    switch (option)
+    do
     {
-        case 0:
-            Console.WriteLine("Thanks for use this app. Good Bye!"); break;
-        case 1:
-        {
-            animal = new Dog("Salchicha", "Jose");
-            Console.WriteLine(animal.Talk());
-        } break;
-        case 2:
-        {
-            animal = new Cat("Skoke", "minino");
-            Console.WriteLine(animal.Talk());
-        } break;
-        case 3:
-        {
-            animal = new Rooster("Kiriki", "Gallito");
-            Console.WriteLine(animal.Talk());
-        } break;
-        case 4:
-        {
-            animal = new Cow("Holstein", "Otis");
-            Console.WriteLine(animal.Talk());
-        } break;
-        default:
-            Console.WriteLine("Invalid Option"); break;
-    }
+        Console.WriteLine(
+            "Please select a option\n" +
+            "1 ---- Dog\n" +
+            "2 ---- Cat \n" +
+            "3 ---- Rooster\n" +
+            "4 ---- Cow\n" +
+            "0 ---- Exit"
+        );
+        
+        option = int.Parse(Console.ReadLine());
+        
+        Console.Clear();
     
-    
-    Thread.Sleep(5000);
-    
-    Console.Clear();
-} while (option != 0);
+        switch (option)
+        {
+            case 0:
+                Console.WriteLine("Thanks for use this app. Good Bye!"); break;
+            case 1:
+            {
+                animal = new Dog("Salchicha", "Pluto");
+                Console.WriteLine(animal.Talk());
+            } break;
+            case 2:
+            {
+                animal = new Cat("Skoke", "Cat Whit Boots");
+                Console.WriteLine(animal.Talk());
+            } break;
+            case 3:
+            {
+                animal = new Rooster("Kiriki", "Claudio");
+                Console.WriteLine(animal.Talk());
+            } break;
+            case 4:
+            {
+                animal = new Cow("Holstein", "Otis");
+                Console.WriteLine(animal.Talk());
+            } break;
+            default:
+                Console.WriteLine("Invalid Option"); break;
+        }
+        
+        
+        Thread.Sleep(5000);
+        
+        Console.Clear();
+    } while (option != 0);
+}
+
